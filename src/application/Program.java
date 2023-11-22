73e6b00abd337a739f88e9a0c0266ecba9441469
Marcele Aiache <tuxmarcele@gmail.com>
2023-11-21 23:03:38 -0300

    Initial commit

---
+++ src/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import entities.Product;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner leia = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter product data: ");
+        System.out.println("Name: ");
+        product.name = leia.nextLine();
+
+        System.out.println("Price: ");
+        product.price = leia.nextDouble();
+
+        System.out.println("Quantity in stock: ");
+        product.quantity = leia.nextInt();
+
+        System.out.println(product.name + ", " + product.price + ", " + product.quantity);
+
+        leia.close();
+    }
+}
